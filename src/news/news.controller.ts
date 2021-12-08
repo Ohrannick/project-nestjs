fd23b6113d45e6892330ac644a4f8519afe71df9
Ohrannick <76492189+Ohrannick@users.noreply.github.com>
2021-12-08 22:50:20 +0300

    lesson 1 - base

---
+++ src/news/news.controller.ts
@@ -0,0 +1,26 @@
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { News, NewsService } from './news.service';
+
+@Controller('news')
+export class NewsController {
+  constructor(private readonly newsService: NewsService) {
+  }
+
+  @Get('/:id')
+  get(@Param('id') id: string): News { 
+    let idInt = parseInt(id);
+    return this.newsService.find(idInt);
+  }
+
+  @Post()
+  create(@Body() news: News): News { 
+    return this.newsService.create(news);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') id: string): string {
+    const idInt = parseInt(id);
+    const isRemoved = this.newsService.remove(idInt);
+    return isRemoved ? 'Новость удалена' : 'Передан неверный идентификатор';
+  }
+}
---
+++ src/news/news.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@nestjs/common';
+
+export interface News {
+  id?: number;
+  title: string;
+  description: string;
+  author: string;
+  countViews?: number;
+}
+
+@Injectable()
+export class NewsService {
+  private readonly news: News[] = [
+    {
+      id: 1,
+      title: 'Наша первая новостя', 
+      description: 'Урра!!!', 
+      author: 'Ohrannick', 
+      countViews: 12
+    }
+  ];
+
+  create(news: News): News {
+    const id = Date.now()
+    const finalNews = {
+      ...news,
+      id: id,
+    }
+    this.news.push(finalNews);
+    return finalNews;
+  }
+
+  find(id: News['id']): News | undefined {
+    return this.news.find((news: News) => news.id === id)
+  }
+
+  remove(id: News['id']): Boolean {
+    const indexRemoveNews = this.news.findIndex((news: News) => news.id === id);
+    if(indexRemoveNews !== -1) {
+      this.news.splice(indexRemoveNews, 1);
+      return true
+    }
+    return false
+  }
+
+
+}
